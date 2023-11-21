c55809c030655ccd3978a9ed1dfef455c664b19a
sukran karakaya <sukrankrky184@gmail.com>
2023-11-21 03:27:14 +0300

    2. Ders Commits

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -3,8 +3,9 @@ import classNames from "classnames"
 import PropsTypes from "prop-types"
 export default function Button({size,children}){
     return createElement('button',{
-        className: classNames("bg-[#1d9bf0] rounded-full font-bold flex items-center hover:bg-[#0000] transition-colors",{
-            "px-4 h-9 ": size ==="normal"
+        className: classNames("bg-[#1d9bf0] rounded-full justify-center font-bold flex items-center hover:bg-[#1a8cd8] transition-colors",{
+            "px-4 h-9 ": size ==="normal",
+            "px-4 h-[52px] w-full text-[17px] ": size=== "large"
 
         })
     }, children)
